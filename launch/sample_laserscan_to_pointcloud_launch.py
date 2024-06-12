15ae4582cf90c72b4f562d48f5dd97bb227d9a3f
mini01 <liam.jemmeson.ki@futu-re.co.jp>
2024-06-12 09:10:38 +0900

    Fixed arguments for static_transform_publisher

--- launch/sample_laserscan_to_pointcloud_launch.py
+++ launch/sample_laserscan_to_pointcloud_launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='static_transform_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'scan']
+            arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'scan']    # 20240612FTR modified
         ),
         Node(
             package='pointcloud_to_laserscan',
